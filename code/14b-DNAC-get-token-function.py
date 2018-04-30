68614cf900c9aaa67508311bebd212dfcef9a820
Ismael <ismaa10@gmail.com>
2018-04-30 15:43:59 -0300

    Se agrega la url como parametro opcional

--- code/14b-DNAC-get-token-function.py
+++ code/14b-DNAC-get-token-function.py
@@ -10,13 +10,13 @@ import json       # Import JSON encoder and decode module
 
 requests.packages.urllib3.disable_warnings()  # Disable warnings
 
+# Hardcoded URL data
+dnac_ip = "sandboxdnac.cisco.com"
+version = "v1"
+post_url = "https://" + dnac_ip + "/api/system/" + version + "/auth/token"
 
-def get_token(username, password):
-    # Hardcoded data
-    dnac_ip = "sandboxdnac.cisco.com"
-    version = "v1"
-    post_url = "https://" + dnac_ip + "/api/system/" + version + "/auth/token"
 
+def get_token(username, password, url=post_url):
     # All DNAC REST API request and response content type is JSON.
     headers = {'content-type': 'application/json'}
 
--- code/soluciones/14b-DNAC-get-token-function.py
+++ code/soluciones/14b-DNAC-get-token-function.py
@@ -10,18 +10,18 @@ import json       # Import JSON encoder and decode module
 
 requests.packages.urllib3.disable_warnings()  # Disable warnings
 
+# Hardcoded URL data
+dnac_ip = "sandboxdnac.cisco.com"
+version = "v1"
+post_url = "https://" + dnac_ip + "/api/system/" + version + "/auth/token"
 
-def get_token(username, password):
-    # Hardcoded data
-    dnac_ip = "sandboxdnac.cisco.com"
-    version = "v1"
-    post_url = "https://" + dnac_ip + "/api/system/" + version + "/auth/token"
 
+def get_token(username, password, url=post_url):
     # All DNAC REST API request and response content type is JSON.
     headers = {'content-type': 'application/json'}
 
     # Make request and get response - "resp" is the response of this request
-    resp = requests.post(post_url, auth=HTTPBasicAuth(
+    resp = requests.post(url, auth=HTTPBasicAuth(
         username=username, password=password), headers=headers, verify=False)
 
     return resp.json()['Token']
